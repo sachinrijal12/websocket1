780189fd454dc4c512444a24c93dbc012e14d8bd
sachinrijal12 <rijalsachin58@gmail.com>
2026-05-01 16:16:51 +0545

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const http = require("http");
+const express = require("express");
+const path = require('path');
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+//web socket.io
+io.on("connection", (socket) => {
+    socket.on("user-message", (message) => {
+        io.emit("message", message);
+    });
+});
+
+app.use(express.static(path.resolve("./public")));
+app.get("/", (req, res) => {
+    return res.sendFile("./public/index.html");
+});
+
+server.listen(8000, () => console.log(`server is stated at 8000 Port `));
